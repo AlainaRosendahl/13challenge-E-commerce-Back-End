fce4f0fac4e3b47c995fb27310897fc2966da040
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-06-18 08:39:15 -0500

    server conn

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
+const categoryRoutes = require('./category-routes.js');
+const productRoutes = require('./product-routes.js');
+const tagRoutes = require('./tag-routes.js');
+
+router.use('/categories' , categoryRoutes );
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
